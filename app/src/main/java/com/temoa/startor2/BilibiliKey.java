0fecb075f0db417c4e5385815404e2f8f0066998
Temoa <temoa_mic@hotmail.com>
2016-10-19 16:42:19 +0800

    update .gitignore

--- app/src/main/java/com/temoa/startor2/BilibiliKey.java
+++ app/src/main/java/com/temoa/startor2/BilibiliKey.java
@@ -8,9 +8,9 @@ package com.temoa.startor2;
  */
 public class BilibiliKey {
 
-    public static final String APPKEY = "4ebafd7c4951b366";
-    public static final String APPSEC = "8cb98205e9b2ad3669aad0fce12a4c13";
+    public static final String APPKEY = "";
+    public static final String APPSEC = "";
 
-    public static final String NEW_APPKEY = "452d3958f048c02a";
-    public static final String NEW_APPSEC = "f7c926f549b9becf1c27644958676a21";
+    public static final String NEW_APPKEY = "";
+    public static final String NEW_APPSEC = "";
 }
